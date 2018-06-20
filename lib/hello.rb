6eee5a0fed9891f231089b2069748afcc896551d
Michael Blandford <mdblandford@gmail.com>
2018-06-20 17:27:12 +0000

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,14 +1,14 @@
 def hello_t(array)
   if block_given?
       # block_given? is a boolean
-    i = 0      
+    i = 0
     # i is a counter variable representing each index number in the array
-    while i < array.length 
+    while i < array.length
       yield array[i]
         # this yields each successive index number as we proceed through the loop
-      i = i+1 
-    end 
-  array 
+      i = i+1
+    end
+  array
   else
     puts "Hey! No block was given!"
   end
